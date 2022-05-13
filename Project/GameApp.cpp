c89b028e5603da9108d59e9905df33fad760e19d
c0155 <C0155@oic.jp>
2022-05-13 10:41:58 +0900

    9-1まで

--- Project/GameApp.cpp
+++ Project/GameApp.cpp
@@ -9,6 +9,13 @@
 
 //INCLUDE
 #include	"GameApp.h"
+#include	"Player.h"
+
+CCamera				gcamera;
+CDirectionalLight	glight;
+CPlayer				gplayer;
+//デバッグ表示フラグ
+bool				gddebug;
 
 /*************************************************************************//*!
 		@brief			アプリケーションの初期化
@@ -20,6 +27,25 @@
 MofBool CGameApp::Initialize(void){
 	//リソース配置ディレクトリの設定
 	CUtilities::SetCurrentDirectory("Resource");
+
+	//カメラ初期化
+	gcamera.SetViewPort();
+	gcamera.LookAt(Vector3(0, 6.0f, -17.0f), Vector3(0, 0, -10), Vector3(0, 1, 0));
+	gcamera.PerspectiveFov(MOF_ToRadian(60.0f), 1024.0f / 768.0f, 0.01f, 1000.0f);
+	CGraphicsUtilities::SetCamera(&gcamera);
+
+	//ライト初期化
+	glight.SetDirection(Vector3(-1, -2, 1.5f));
+	glight.SetDiffuse(MOF_COLOR_WHITE);
+	glight.SetAmbient(MOF_COLOR_HWHITE);
+	glight.SetSpeculer(MOF_COLOR_WHITE);
+	CGraphicsUtilities::SetDirectionalLight(&glight);
+
+	//プレイヤーの素材読み込み
+	gplayer.Load();
+
+	//プレイヤーの初期化
+	gplayer.Initialize();
 	
 	return TRUE;
 }
@@ -33,6 +59,24 @@ MofBool CGameApp::Initialize(void){
 MofBool CGameApp::Update(void){
 	//キーの更新
 	g_pInput->RefreshKey();
+
+	//プレイヤーの更新
+	gplayer.Update();
+	//デバッグ表示の切り替え
+	if (g_pInput->IsKeyPush(MOFKEY_F1))
+	{
+		gddebug = ((gddebug) ? false : true);
+	}
+
+	//プレイヤーの動きに合わせてカメラを動かす
+	float posx = gplayer.GetPosition().x * 0.4f;
+	CVector3 cpos = gcamera.GetViewPosition();
+	CVector3 tpos = gcamera.GetTargetPosition();
+	CVector3 vup = Vector3(0, 1, 0);
+	cpos.x = posx;
+	tpos.x = posx;
+	gcamera.LookAt(cpos, tpos, vup);
+	gcamera.Update();
 	return TRUE;
 }
 
@@ -49,6 +93,24 @@ MofBool CGameApp::Render(void){
 	// 画面のクリア
 	g_pGraphics->ClearTarget(0.65f,0.65f,0.67f,0.0f,1.0f,0);
 
+	g_pGraphics->SetDepthEnable(TRUE);
+
+	gplayer.Render();
+
+	if (gddebug)
+	{
+		CMatrix44 matworld;
+		matworld.Scaling(FIELD_HALF_X * 2, 1, FIELD_HALF_Z * 2);
+		CGraphicsUtilities::RenderPlane(matworld, Vector4(1, 1, 1, 0.4f));
+	}
+
+	g_pGraphics->SetDepthEnable(FALSE);
+
+	if (gddebug)
+	{
+		gplayer.RenderDebugText();
+	}
+
 	// 描画の終了
 	g_pGraphics->RenderEnd();
 	return TRUE;
@@ -61,5 +123,6 @@ MofBool CGameApp::Render(void){
 						それ以外	失敗、エラーコードが戻り値となる
 *//**************************************************************************/
 MofBool CGameApp::Release(void){
+	gplayer.Release();
 	return TRUE;
 }
--- Project/Player.cpp
+++ Project/Player.cpp
@@ -20,6 +20,10 @@ CPlayer::~CPlayer(){
  */
 bool CPlayer::Load(void){
 	// メッシュの読み込み
+	if (!m_Mesh.Load("player.mom"))
+	{
+		return false;
+	}
 	return true;
 }
 
@@ -27,18 +31,58 @@ bool CPlayer::Load(void){
  * 初期化
  */
 void CPlayer::Initialize(void){
+	m_Pos = Vector3(0.0f, 0.0f, -FIELD_HALF_Z + 2.0f);
+	m_RotZ = 0;
 }
 
 /**
  * 更新
  */
-void CPlayer::Update(void){
+void CPlayer::Update(void) {
+	//回転方向
+	float roll = 0;
+	//キーボードでの移動
+	if (g_pInput->IsKeyHold(MOFKEY_LEFT))
+	{
+		m_Pos.x = max(m_Pos.x - PLAYER_SPEED, -FIELD_HALF_X);
+		roll -= MOF_MATH_PI;
+	}
+	if (g_pInput->IsKeyHold(MOFKEY_RIGHT))
+	{
+		m_Pos.x = min(m_Pos.x + PLAYER_SPEED, FIELD_HALF_X);
+		roll += MOF_MATH_PI;
+	}
+	if (g_pInput->IsKeyHold(MOFKEY_UP))
+	{
+		m_Pos.z = min(m_Pos.z + PLAYER_SPEED, FIELD_HALF_Z);
+	}
+	if (g_pInput->IsKeyHold(MOFKEY_DOWN))
+	{
+		m_Pos.z= max(m_Pos.z - PLAYER_SPEED, -FIELD_HALF_Z);
+	}
+	//回転
+	float rotspeed = MOF_ToRadian(10);
+	if (roll == 0)
+	{
+		rotspeed = min(abs(m_RotZ) * 0.1f, rotspeed);
+	}
+	if (abs(m_RotZ) <= rotspeed || signbit(m_RotZ) != signbit(roll))
+	{
+		m_RotZ += roll;
+	}
+	m_RotZ -= copysignf(min(rotspeed, abs(m_RotZ)), m_RotZ);
 }
 
 /**
  * 描画
  */
 void CPlayer::Render(void){
+	//ワールド行列作成
+	CMatrix44 matworld;
+	matworld.RotationZ(m_RotZ);
+	matworld.SetTranslation(m_Pos);
+	//メッシュの描画
+	m_Mesh.Render(matworld);
 }
 
 /**
@@ -54,4 +98,5 @@ void CPlayer::RenderDebugText(void){
  * 解放
  */
 void CPlayer::Release(void){
+	m_Mesh.Release();
 }
